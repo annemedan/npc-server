52bbae881379c5bc63b826711bd0decc0d48fbae
Anne Dantas <annedantas@gmail.com>
2021-12-16 12:47:33 +0000

    changing the default image on the item model

--- models/item.model.js
+++ models/item.model.js
@@ -16,8 +16,7 @@ const itemSchema = new mongoose.Schema({
   },
   productImage: {
     type: String,
-    default:
-      "https://png.vector.me/files/images/1/9/191329/blue_gift_clip_art.jpg",
+    default: "https://static.thenounproject.com/png/908418-200.png",
   },
   quantity_available: { type: Number },
   price: { type: Number, trim: true, required: true, maxlength: 10 },
